e824bcf16dacb4f32b70f0550ddf11449bff653d
Jakob Nyberg <jaknyb@kth.se>
2024-03-13 13:58:00 +0100

    add gym api check (not passing ATM)

--- tests/api_test.py
+++ tests/api_test.py
@@ -9,34 +9,61 @@ from maltoolbox.model import model as malmodel
 from pettingzoo.test import parallel_api_test
 
 from malpzsim.sims.mal_petting_zoo_simulator import MalPettingZooSimulator
+import gymnasium as gym
+from gymnasium.utils import env_checker
 
-logger = logging.getLogger(__name__)
+from malpzsim.wrappers.gym_wrapper import AttackerEnv, DefenderEnv
 
-null_action = (0, None)
 
-lang_file = "org.mal-lang.coreLang-1.0.0.mar"
-lang_spec = specification.load_language_specification_from_mar(lang_file)
-specification.save_language_specification_to_json(lang_spec, "lang_spec.json")
-lang_classes_factory = classes_factory.LanguageClassesFactory(lang_spec)
-lang_classes_factory.create_classes()
+def test_pz():
+    logger = logging.getLogger(__name__)
 
-lang_graph = mallanguagegraph.LanguageGraph()
-lang_graph.generate_graph(lang_spec)
+    null_action = (0, None)
 
-model = malmodel.Model("Test Model", lang_spec, lang_classes_factory)
-model.load_from_file("example_model.json")
+    lang_file = "tests/org.mal-lang.coreLang-1.0.0.mar"
+    lang_spec = specification.load_language_specification_from_mar(lang_file)
+    specification.save_language_specification_to_json(lang_spec, "tests/lang_spec.json")
+    lang_classes_factory = classes_factory.LanguageClassesFactory(lang_spec)
+    lang_classes_factory.create_classes()
 
-attack_graph = malattackgraph.AttackGraph()
-attack_graph.generate_graph(lang_spec, model)
-attack_graph.attach_attackers(model)
-attack_graph.save_to_file("tmp/attack_graph.json")
+    lang_graph = mallanguagegraph.LanguageGraph()
+    lang_graph.generate_graph(lang_spec)
 
-env = MalPettingZooSimulator(lang_graph, model, attack_graph, max_iter=5)
+    model = malmodel.Model("Test Model", lang_spec, lang_classes_factory)
+    model.load_from_file("tests/example_model.json")
 
-env.register_attacker("attacker", 0)
-env.register_defender("defender")
+    attack_graph = malattackgraph.AttackGraph()
+    attack_graph.generate_graph(lang_spec, model)
+    attack_graph.attach_attackers(model)
+    attack_graph.save_to_file("tmp/attack_graph.json")
 
-logger.debug("Run Parrallel API test.")
-parallel_api_test(env, num_cycles=50)
+    env = MalPettingZooSimulator(lang_graph, model, attack_graph, max_iter=5)
 
-env.close()
+    env.register_attacker("attacker", 0)
+    env.register_defender("defender")
+
+    logger.debug("Run Parrallel API test.")
+    parallel_api_test(env, num_cycles=50)
+
+    env.close()
+
+
+def test_gym():
+    gym.register("MALDefenderEnv-v0", entry_point=DefenderEnv)
+    env = gym.make(
+        "MALDefenderEnv-v0",
+        model_file="tests/example_model.json",
+        lang_file="tests/org.mal-lang.coreLang-1.0.0.mar",
+        unholy=True,
+    )
+    
+    env_checker.check_env(env.unwrapped)
+
+    gym.register("MALAttackerEnv-v0", entry_point=AttackerEnv)
+    env = gym.make(
+        "MALAttackerEnv-v0",
+        model_file="tests/example_model.json",
+        lang_file="tests/org.mal-lang.coreLang-1.0.0.mar",
+    )
+    
+    env_checker.check_env(env.unwrapped)
