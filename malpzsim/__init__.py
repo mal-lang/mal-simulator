3e33d88c56755a7f5f9cf62f49e07f5ffd77f314
Andrei Buhaiu <buhaiu@kth.se>
2024-03-15 18:00:07 +0100

    Bump version to 0.0.14.

--- malpzsim/__init__.py
+++ malpzsim/__init__.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-# MAL Petting Zoo Simulator v0.0.13
+# MAL Petting Zoo Simulator v0.0.14
 # Copyright 2024, Andrei Buhaiu.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -23,7 +23,7 @@ MAL Petting Zoo Simulator
 """
 
 __title__ = "malpzsim"
-__version__ = "0.0.13"
+__version__ = "0.0.14"
 __authors__ = ["Andrei Buhaiu", "Jakob Nyberg"]
 __license__ = "Apache 2.0"
 __docformat__ = "restructuredtext en"
