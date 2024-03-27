dcb3c8971c29d82c24b3bb930df30979da4a83bc
Andrei Buhaiu <buhaiu@kth.se>
2024-03-27 14:00:46 +0100

    Update wrapper to take attack graph as an optional parameter.

--- malpzsim/wrappers/wrapper.py
+++ malpzsim/wrappers/wrapper.py
@@ -13,8 +13,13 @@ class LazyWrapper(ParallelEnv):
     def __init__(self, **kwargs):
         lang_file = kwargs.pop("lang_file")
         model_file = kwargs.pop("model_file")
+        if "attack_graph_file" in kwargs:
+            attack_graph_file = kwargs.pop("attack_graph_file")
+        else:
+            attack_graph_file = ""
         agents = kwargs.pop("agents", {})
-        lang_spec = specification.load_language_specification_from_mar(str(lang_file))
+        lang_spec = specification.load_language_specification_from_mar(
+            str(lang_file))
         lang_classes_factory = LanguageClassesFactory(lang_spec)
         lang_classes_factory.create_classes()
 
@@ -23,18 +28,28 @@ class LazyWrapper(ParallelEnv):
         model = Model("Test Model", lang_spec, lang_classes_factory)
         model.load_from_file(model_file)
 
-        attack_graph = AttackGraph(lang_spec, model)
-        attack_graph.attach_attackers(model)
+        if attack_graph_file != "":
+            # If we were provided with an attack graph file we load it.
+            attack_graph = AttackGraph()
+            attack_graph.load_from_file(filename = attack_graph_file,
+                model = model)
+            attack_graph.attach_attackers(model)
+        else:
+            # Otherwise we generate the attack graph based on the model
+            # provided.
+            attack_graph = AttackGraph(lang_spec, model)
+            attack_graph.attach_attackers(model)
 
-        sim = MalPettingZooSimulator(lang_graph, model, attack_graph, **kwargs)
+        sim = MalPettingZooSimulator(lang_graph,
+            model,
+            attack_graph,
+            **kwargs)
 
         for agent_class, agent_id in agents.items():
             if agent_class == "attacker":
                 sim.register_attacker(agent_id, 0)
             elif agent_class == "defender":
                 sim.register_defender(agent_id)
-
-        # TODO - This is a temporary fix to set the rewards for the nodes in the attack graph
 
         self.sim = sim
 
@@ -49,6 +64,14 @@ class LazyWrapper(ParallelEnv):
         self, seed: int | None = None, options: dict | None = None
     ) -> tuple[dict, dict[Any, dict]]:
         result = self.sim.reset(seed, options)
+        # self._hack_rewards()
+        return result
+
+    def _hack_rewards(self):
+        """
+        HACK: Hack function to add rewards to test attack graph.
+        TODO: Remove when rewards are properly handled.
+        """
         sim = self.sim
         sim.attack_graph.get_node_by_id("Application:0:notPresent").reward = 2
         sim.attack_graph.get_node_by_id("Application:0:supplyChainAuditing").reward = 7
@@ -67,7 +90,6 @@ class LazyWrapper(ParallelEnv):
         sim.attack_graph.get_node_by_id("Application:1:fullAccess").reward = 2
         sim.attack_graph.get_node_by_id("Identity:5:assume").reward = 2
         sim.attack_graph.get_node_by_id("Application:7:fullAccess").reward = 6
-        return result
 
     def observation_space(self, agent: Any) -> Space:
         return self.sim.observation_space(agent)
