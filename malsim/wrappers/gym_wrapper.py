7a858505b1c7d2c3b39c5384fcd4ff0e0477b22e
Joakim Loxdal <loxdal@kth.se>
2024-09-11 15:52:55 +0200

    Replace the LazyWrapper with create_simulator_from_scenario
    Remove LazyWrapper, use 'create_simulator_from_scenario' instead.
    Use scenario file as input instead of lang+model+graph.
    Move responsibility over agent ids to MalSimulator and scenario loading.
    Make sure all instance variables are defined in __init__ of the env wrappers.

--- malsim/wrappers/gym_wrapper.py
+++ malsim/wrappers/gym_wrapper.py
@@ -7,106 +7,139 @@ from gymnasium import spaces
 from gymnasium.core import RenderFrame
 import numpy as np
 
-from malsim.wrappers.wrapper import LazyWrapper
-from malsim.agents import searchers
-
-
-AGENT_ATTACKER = "attacker"
-AGENT_DEFENDER = "defender"
+from ..scenario import create_simulator_from_scenario
 
 
 class AttackerEnv(gym.Env):
     metadata = {"render_modes": []}
 
-    def __init__(self, **kwargs: Any) -> None:
+    def __init__(self, scenario_file: str, **kwargs) -> None:
+        """
+        Params:
+        - scenario_file: the scenario that should be loaded
+                         and made into a MalSimulator
+        """
+
         self.render_mode = kwargs.pop("render_mode", None)
-        agents = {AGENT_ATTACKER: AGENT_ATTACKER}
-        self.env = LazyWrapper(agents=agents, **kwargs)
-        self.observation_space = self.env.observation_space(AGENT_ATTACKER)
-        self.action_space = self.env.action_space(AGENT_ATTACKER)
+
+        # Create a simulator from the scenario given
+        self.sim, _ = create_simulator_from_scenario(scenario_file, **kwargs)
+
+        # Use first attacker as attacker agent in simulation
+        # since only single agent is currently supported
+        self.attacker_agent_id = list(self.sim.get_attacker_agents().keys())[0]
+        self.observation_space = self.sim.observation_space(self.attacker_agent_id)
+        self.action_space = self.sim.action_space(self.attacker_agent_id)
         super().__init__()
 
     def reset(
         self, *, seed: int | None = None, options: dict[str, Any] | None = None
     ) -> tuple[Any, dict[str, Any]]:
         super().reset(seed=seed, options=options)
-        obs, info = self.env.reset(seed=seed, options=options)
-        return obs[AGENT_ATTACKER], info[AGENT_ATTACKER]
+
+        obs, info = self.sim.reset(seed=seed, options=options)
+        return obs[self.attacker_agent_id], info[self.attacker_agent_id]
 
     def step(
         self, action: Any
     ) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
         obs: Dict[str, Any]
-        obs, rewards, terminated, truncated, infos = self.env.step(
-            {AGENT_ATTACKER: action}
-        )
+
+        # TODO: Add potential defender and give defender action if it exists
+        actions = {
+            self.attacker_agent_id: action,
+        }
+        obs, rewards, terminated, truncated, infos = self.sim.step(actions)
         return (
-            obs[AGENT_ATTACKER],
-            rewards[AGENT_ATTACKER],
-            terminated[AGENT_ATTACKER],
-            truncated[AGENT_ATTACKER],
-            infos[AGENT_ATTACKER],
+            obs[self.attacker_agent_id],
+            rewards[self.attacker_agent_id],
+            terminated[self.attacker_agent_id],
+            truncated[self.attacker_agent_id],
+            infos[self.attacker_agent_id],
         )
 
     def render(self):
-        return self.env.render()
+        return self.sim.render()
 
     @property
     def num_assets(self):
-        return self.env.sim.num_assets
+        return self.sim.num_assets
 
     @property
     def num_step_names(self):
-        return self.env.sim.num_step_names
+        return self.sim.num_step_names
 
 
 class DefenderEnv(gym.Env):
     metadata = {"render_modes": []}
 
-    def __init__(self, **kwargs: Dict[str, Any]) -> None:
-        attacker_class: str = str(kwargs.pop("attacker_class", "BreadthFirstAttacker"))
+    def __init__(
+            self, scenario_file, **kwargs
+        ) -> None:
         self.randomize = kwargs.pop("randomize_attacker_behavior", False)
         self.render_mode = kwargs.pop("render_mode", None)
-        agents = {AGENT_ATTACKER: AGENT_ATTACKER, AGENT_DEFENDER: AGENT_DEFENDER}
-        self.env = LazyWrapper(agents=agents, **kwargs)
-        self.attacker_class = searchers.AGENTS[attacker_class]
-        self.observation_space = self.env.observation_space(AGENT_DEFENDER)
-        self.action_space = self.env.action_space(AGENT_DEFENDER)
+
+        self.sim, conf = create_simulator_from_scenario(scenario_file, **kwargs)
+
+        # Select first attacker and first defender for the simulation
+        # currently only one of each agent is supported
+        self.attacker_agent_id = list(self.sim.get_attacker_agents().keys())[0]
+        self.defender_agent_id = list(self.sim.get_defender_agents().keys())[0]
+
+        self.attacker_class = conf['agents'][self.attacker_agent_id]['agent_class']
+        self.attacker = self.attacker_class({})
+
+        self.observation_space = self.sim.observation_space(self.defender_agent_id)
+        self.action_space = self.sim.action_space(self.defender_agent_id)
+
+        self.attacker_obs = None
+        self.attacker_mask = None
+
 
     def reset(
         self, *, seed: int | None = None, options: dict[str, Any] | None = None
     ) -> tuple[Any, dict[str, Any]]:
+
         super().reset(seed=seed, options=options)
-        self.attacker = self.attacker_class({"seed": seed, "randomize": self.randomize})
-        obs, info = self.env.reset(seed=seed, options=options)
 
-        self.attacker_obs = obs[AGENT_ATTACKER]
-        self.attacker_mask = info[AGENT_ATTACKER]["action_mask"]
+        self.attacker = self.attacker_class(
+            {"seed": seed, "randomize": self.randomize}
+        )
 
-        return obs[AGENT_DEFENDER], info[AGENT_DEFENDER]
+        obs, info = self.sim.reset(seed=seed, options=options)
+
+        self.attacker_obs = obs[self.attacker_agent_id]
+        self.attacker_mask = info[self.attacker_agent_id]["action_mask"]
+
+        return obs[self.defender_agent_id], info[self.defender_agent_id]
 
     def step(
         self, action: Any
     ) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+
         attacker_action = self.attacker.compute_action_from_dict(
             self.attacker_obs, self.attacker_mask
         )
-        obs: Dict[str, Any]
-        obs, rewards, terminated, truncated, infos = self.env.step(
-            {AGENT_DEFENDER: action, AGENT_ATTACKER: attacker_action}
-        )
-        self.attacker_obs = obs[AGENT_ATTACKER]
-        self.attacker_mask = infos[AGENT_ATTACKER]["action_mask"]
+
+        actions = {
+            self.defender_agent_id: action,
+            self.attacker_agent_id: attacker_action
+        }
+        obs, rewards, terminated, truncated, infos = self.sim.step(actions)
+
+        self.attacker_obs = obs[self.attacker_agent_id]
+        self.attacker_mask = infos[self.attacker_agent_id]["action_mask"]
+
         return (
-            obs[AGENT_DEFENDER],
-            rewards[AGENT_DEFENDER],
-            terminated[AGENT_DEFENDER],
-            truncated[AGENT_DEFENDER],
-            infos[AGENT_DEFENDER],
+            obs[self.defender_agent_id],
+            rewards[self.defender_agent_id],
+            terminated[self.defender_agent_id],
+            truncated[self.defender_agent_id],
+            infos[self.defender_agent_id],
         )
 
     def render(self):
-        return self.env.render()
+        return self.sim.render()
 
     @staticmethod
     def add_reverse_edges(edges: np.ndarray, defense_steps: set) -> np.ndarray:
@@ -121,11 +154,11 @@ class DefenderEnv(gym.Env):
 
     @property
     def num_assets(self):
-        return self.env.sim.num_assets
+        return self.sim.num_assets
 
     @property
     def num_step_names(self):
-        return self.env.sim.num_step_names
+        return self.sim.num_step_names
 
 
 def _to_binary(val, max_val):
@@ -205,16 +238,13 @@ if __name__ == "__main__":
     gym.register("MALDefenderEnv-v0", entry_point=DefenderEnv)
     env = gym.make(
         "MALDefenderEnv-v0",
-        model_file="/storage/GitHub/mal-simulator/tests/example_model.json",
-        lang_file="/storage/GitHub/mal-simulator/tests/org.mal-lang.coreLang-1.0.0.mar",
-        unholy=True,
+        scenario_file="tests/testdata/scenarios/simple_scenario.yml",
     )
     env_checker.check_env(env.unwrapped)
 
     gym.register("MALAttackerEnv-v0", entry_point=AttackerEnv)
     env = gym.make(
         "MALAttackerEnv-v0",
-        model_file="/storage/GitHub/mal-simulator/tests/example_model.json",
-        lang_file="/storage/GitHub/mal-simulator/tests/org.mal-lang.coreLang-1.0.0.mar",
+        scenario_file="tests/testdata/scenarios/no_defender_agent_scenario.yml",
     )
     env_checker.check_env(env.unwrapped)
