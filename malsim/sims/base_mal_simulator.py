cc8ab1a5e5084a0c072a49cec774110584c93156
Joakim Loxdal <loxdal@kth.se>
2024-11-26 16:06:03 +0100

    Add Base MAL Simulator
    - A Class with basic simulator functionality
    - Works only with the attack graph
    - Enums for actions, action states and agents
    - No ML stuff or serialization

---
+++ malsim/sims/base_mal_simulator.py
@@ -0,0 +1,303 @@
+from __future__ import annotations
+
+import copy
+from dataclasses import dataclass, field
+import logging
+from enum import Enum
+from typing import Optional
+
+from maltoolbox.attackgraph import AttackGraph, AttackGraphNode
+from maltoolbox.attackgraph.analyzers import apriori
+from maltoolbox.attackgraph import query
+
+from .mal_simulator_settings import MalSimulatorSettings
+
+ITERATIONS_LIMIT = int(1e9)
+
+logger = logging.getLogger(__name__)
+
+class AgentType(Enum):
+    """Enum for agent types"""
+    ATTACKER = 'attacker'
+    DEFENDER = 'defender'
+
+@dataclass
+class SimulatorAgent():
+    """Stores the state of an agent in the simulator"""
+    name: str
+    type: AgentType
+
+    observation: dict = field(default_factory=lambda: {})
+    action_surface: list = field(default_factory=lambda: [])
+    reward: int = 0
+    attacker_id: Optional[int] = None # Only for attacker agent
+
+
+class ActionState(Enum):
+    """The state of an agent action."""
+    WAIT = 0  # Agent is waiting, not acting on any node
+    ACT = 1   # Agent is acting on a specific node
+
+@dataclass
+class AgentAction:
+    """Represents an agent action
+    - If action_state is WAIT, nodes is empty.
+    - If action_state is ACT, nodes is a list of the
+      Attack Graph nodes the agent decided to enable.
+    """
+    agent_state: ActionState = ActionState.WAIT
+    nodes: list[AttackGraphNode] = field(default_factory=lambda: [])
+
+    @classmethod
+    def wait(cls):
+        """Helper to create a wait action"""
+        return cls(ActionState.WAIT)
+
+    @classmethod
+    def act(cls, nodes):
+        """Helper to create an act action"""
+        return cls(ActionState.ACT, nodes)
+
+class BaseMalSimulator():
+    """A simple MAL Simulator that works on the AttackGraph
+
+    Allows user to register agents (defender and attacker)
+    and lets the agents perform actions step by step and updates
+    the state of the attack graph based on the steps chosen.
+    """
+
+    def __init__(
+        self,
+        attack_graph: AttackGraph,
+        prune_unviable_unnecessary: bool = True,
+        sim_settings: MalSimulatorSettings = MalSimulatorSettings(),
+        max_iter=ITERATIONS_LIMIT,
+    ):
+        """
+        Args:
+            lang_graph                  -   The language graph to use
+            model                       -   The model to use
+            attack_graph                -   The attack graph to use
+            max_iter                    -   Max iterations in simulation
+            prune_unviable_unnecessary  -   Prunes graph if set to true
+            sim_settings                -   Settings for simulator
+        """
+        super().__init__()
+        logger.info("Creating Base MAL Simulator.")
+
+        # Calculate viability and necessity and optionally prune graph
+        apriori.calculate_viability_and_necessity(attack_graph)
+        if prune_unviable_unnecessary:
+            apriori.prune_unviable_and_unnecessary_nodes(attack_graph)
+
+        # Keep a backup attack graph to use when resetting
+        self.attack_graph_backup = copy.deepcopy(attack_graph)
+
+        # Initialize all values
+        self.attack_graph = attack_graph
+        self.sim_settings = sim_settings
+        self.max_iter = max_iter # Max iterations before stopping simulation
+        self.cur_iter = 0        # Keep track on current iteration
+        self.agents_dict = {}    # Keep track on registered agents
+        self.agents: list[SimulatorAgent] = []
+
+    def reset(
+            self,
+            seed: Optional[int] = None,
+            options: Optional[dict] = None
+        ):
+        """Reset attack graph, iteration and reinitialize agents"""
+
+        logger.info("Resetting MAL Simulator.")
+        # Reset attack graph
+        self.attack_graph = copy.deepcopy(self.attack_graph_backup)
+        # Reset current iteration
+        self.cur_iter = 0
+        # Reset agents
+        self._reset_agents()
+
+    def _reset_agents(self):
+        """Reset agent rewards and action surfaces"""
+
+        for agent in self.agents:
+            # Reset reward
+            agent.reward = 0
+            agent.observation = {}
+
+            if agent.type == AgentType.ATTACKER:
+                attacker_id = agent.attacker_id
+                try:
+                    attacker = self.attack_graph.attackers[attacker_id]
+                except LookupError as e:
+                    raise LookupError(
+                        f"No attacker at index {attacker_id} in attack graph"
+                    ) from e
+
+                # Reset action surface and observation
+                agent.action_surface = \
+                    query.get_attack_surface(attacker)
+
+            elif agent.type == AgentType.DEFENDER:
+
+                # Reset action surface and observation
+                agent.action_surface = \
+                    query.get_defense_surface(self.attack_graph)
+            else:
+                agent.action_surface = []
+
+    def _update_agent_action_surfaces(self):
+        """Set agent action surfaces according to current state"""
+        for agent in self.agents:
+            if agent.type == AgentType.ATTACKER:
+                attacker_id = agent.attacker_id
+                attacker = self.attack_graph.attackers[attacker_id]
+                # Get current action surface
+                agent.action_surface = \
+                    query.get_attack_surface(attacker)
+
+            elif agent.type == AgentType.DEFENDER:
+                # Get current action surface
+                agent.action_surface = \
+                    query.get_defense_surface(self.attack_graph)
+            else:
+                agent.action_surface = []
+
+    def register_attacker(self, agent_name, attacker_id: int):
+        """Register an attacker agent in the simulator
+        
+        The agent is given a name and an id,
+        the id points to an attacker in the attack graph
+        """
+
+        logger.info(
+            'Register attacker "%s" agent with id %d.',
+            agent_name, attacker_id)
+
+        assert agent_name not in self.agents_dict, \
+            f"Duplicate attacker agent named {agent_name} not allowed"
+
+        agent = SimulatorAgent(
+            agent_name, AgentType.ATTACKER, attacker_id=attacker_id
+        )
+        self.agents.append(agent)
+        self.agents_dict[agent_name] = agent
+
+    def register_defender(self, agent_name):
+        """Add defender agent to the simulator
+        
+        Note:
+        Defenders will be run first so that the defenses prevent attackers
+        appropriately in case any attackers select attack steps that the
+        defenders safeguards against during the same step.
+        """
+
+        logger.info('Register defender "%s" agent.', agent_name)
+        assert agent_name not in self.agents_dict, \
+            f"Duplicate defender agent named {agent_name} not allowed"
+
+        # Add defenders at the start of the list to make
+        # sure they have priority when performing steps.
+        agent = SimulatorAgent(agent_name, AgentType.DEFENDER)
+        self.agents.insert(0, agent)
+        self.agents_dict[agent_name] = agent
+
+    def get_attacker_agents(self) -> SimulatorAgent:
+        """Return list of attacker agents"""
+        return [a for a in self.agents
+                if a.type == AgentType.ATTACKER]
+
+    def get_defender_agents(self) -> SimulatorAgent:
+        """Return list of defender agents"""
+        return [a for a in self.agents
+                if a.type == AgentType.DEFENDER]
+
+    def _attacker_step(
+            self, agent: SimulatorAgent, nodes: list[AttackGraphNode]
+        ) -> list[AttackGraphNode]:
+        """Compromise attack step nodes with attacker
+
+        Args:
+        agent - the agent to compromise nodes with
+        nodes - the nodes to compromise
+
+        Returns: list of attack steps nodes that were compromised
+        """
+
+        enabled_nodes = []
+        attacker_id = agent.attacker_id
+        attacker = self.attack_graph.attackers[attacker_id]
+
+        for node in nodes:
+            # Compromise node if possible
+            if query.is_node_traversable_by_attacker(node, attacker):
+                attacker.compromise(node)
+                agent.reward += node.extras.get("reward", 0)
+                enabled_nodes.append(node)
+
+        return enabled_nodes
+
+    def _defender_step(
+            self, agent: SimulatorAgent, nodes: list[AttackGraphNode]
+        ) -> list[AttackGraphNode]:
+        """Enable defense step nodes with defender
+
+        Args:
+        agent - the agent to activate defense nodes with
+        nodes - the defense step nodes to enable
+
+        Returns: list of defense steps nodes that were enabled
+        """
+
+        enabled_nodes = []
+
+        for node in nodes:
+            # Enable defense if possible
+            if node.is_available_defense():
+                node.defense_status = 1.0
+                node.is_viable = False
+                apriori.propagate_viability_from_unviable_node(node)
+                agent.reward -= node.extras.get("reward", 0)
+                enabled_nodes.append(node)
+
+        return enabled_nodes
+
+    def step(self, actions: dict[str, AgentAction]):
+        """
+        step(action) takes in an action for each agent and should return the
+        - observations
+        - rewards
+        - terminations
+        - truncations
+        - infos
+        dicts where each dict looks like {agent_1: item_1, agent_2: item_2}
+        """
+        logger.debug(
+            "Stepping through iteration %d/%d", self.cur_iter, self.max_iter)
+        logger.debug("Performing actions: %s", actions)
+
+        # Peform agent actions
+        # Note: by design defenders go first, then attackers
+        for agent in self.agents:
+
+            agent_action = actions.get(agent.name, [])
+            self._update_agent_action_surfaces()
+
+            if agent_action.agent_state == ActionState.WAIT:
+                # Agent decided to wait, move on.
+                continue
+
+            if agent.type == AgentType.ATTACKER:
+                self._attacker_step(agent, agent_action.nodes)
+
+            elif agent.type == AgentType.DEFENDER:
+                self._defender_step(agent, agent_action.nodes)
+
+            else:
+                logger.error('Agent %s has unknown type: %s',
+                             agent.name, agent.type)
+
+        self.cur_iter += 1
+
+        return {
+            agent.name: agent for agent in self.agents
+        }
