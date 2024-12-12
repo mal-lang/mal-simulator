6e55457672f32e8598e2cfd6632b31fad1604dab
Joakim Loxdal <loxdal@kth.se>
2024-12-12 16:08:03 +0100

    Make MalSimulator a pure simulator instead of ParallelEnv
    - The simulator now only does simulation stuff
    - Remove all observation related code, simplify the simulator
    - Agents are registered as MalSimAgent objects instead of just strings

--- malsim/sims/__init__.py
+++ malsim/sims/__init__.py
@@ -1,2 +1,8 @@
-from .mal_simulator import MalSimulator as MalSimulator
-from .mal_simulator_settings import MalSimulatorSettings as MalSimulatorSettings
+# from .mal_simulator import MalSimulator as MalSimulator
+from .mal_sim_settings import MalSimulatorSettings
+from .mal_simulator import MalSimulator
+
+__all__ = [
+    'MalSimulatorSettings',
+    'MalSimulator'
+]
