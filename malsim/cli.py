fc2d58cff270b0a5920760e644694fde08896a4a
Joakim Loxdal <loxdal@kth.se>
2024-12-12 16:10:18 +0100

    Change how agents are loaded from scenario file
    - User can now give any number of agents
    - They have a class (and entrypoints if they are attackers)

--- malsim/cli.py
+++ malsim/cli.py
@@ -4,110 +4,11 @@ from __future__ import annotations
 import argparse
 import logging
 
-from .sims.mal_simulator import MalSimulator
-from .agents.keyboard_input import KeyboardAgent
-from .scenario import create_simulator_from_scenario
+from .envs.cli_env import CLIEnv
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logging.getLogger().setLevel(logging.INFO)
-
-
-def run_simulation(sim: MalSimulator, sim_config: dict):
-    """Run a simulation on an attack graph with given config"""
-
-    # Constants
-    NULL_ACTION = (0, None)
-
-    attacker_agent_id = next(iter(sim.get_attacker_agents()))
-    defender_agent_id = next(iter(sim.get_defender_agents()), None)
-
-    reverse_vocab = sim._index_to_full_name
-
-    # Initialize defender and attacker according to classes
-    defender_class = sim_config['agents'][defender_agent_id]['agent_class']\
-                     if defender_agent_id else None
-    defender_agent = (defender_class(reverse_vocab)
-                          if defender_class == KeyboardAgent
-                          else defender_class({})
-                      if defender_class
-                      else None)
-
-    attacker_class = sim_config['agents'][attacker_agent_id]['agent_class']
-    attacker_agent = (attacker_class(reverse_vocab)
-                      if attacker_class == KeyboardAgent
-                      else attacker_class({}))
-
-    obs, infos = sim.reset()
-    done = False
-
-    logger.info("Starting game.")
-
-    total_reward_defender = 0
-    total_reward_attacker = 0
-
-    while not done:
-
-        defender_action = NULL_ACTION
-        if defender_agent:
-            defender_action = defender_agent.compute_action_from_dict(
-                obs[defender_agent_id],
-                infos[defender_agent_id]["action_mask"]
-            )
-
-        attacker_action = attacker_agent.compute_action_from_dict(
-            obs[attacker_agent_id],
-            infos[attacker_agent_id]["action_mask"]
-        )
-
-        if attacker_action[1] is not None:
-            logger.info(
-                "Attacker Action: %s", reverse_vocab[attacker_action[1]])
-        else:
-            logger.info("Attacker Action: None")
-            # Stop the attacker if it has run out of things to do since
-            # the experiment cannot progress any further.
-            done = True
-
-        action_dict = {
-            attacker_agent_id: attacker_action,
-            defender_agent_id: defender_action
-        }
-
-        # Perform next step of simulation
-        obs, rewards, terminated, truncated, infos = sim.step(action_dict)
-
-        logger.debug(
-            "Attacker has compromised the following attack steps so far:"
-        )
-        attacker_obj = sim.attack_graph.attackers[
-            sim.agents_dict[attacker_agent_id]["attacker"]
-        ]
-        for step in attacker_obj.reached_attack_steps:
-            logger.debug(step.id)
-
-        logger.info("Attacker Reward: %s", rewards.get(attacker_agent_id))
-
-        if defender_agent:
-            logger.info("Defender Reward: %s", rewards.get(defender_agent_id))
-
-        total_reward_defender += rewards.get(defender_agent_id, 0) if defender_agent else 0
-        total_reward_attacker += rewards.get(attacker_agent_id, 0)
-
-        done |= terminated.get(attacker_agent_id, True) or truncated.get(attacker_agent_id, True)
-
-        print("---\n")
-
-    logger.info("Game Over.")
-
-    if defender_agent:
-        logger.info("Total Defender Reward: %s", total_reward_defender)
-    logger.info("Total Attacker Reward: %s", total_reward_attacker)
-
-    print("Press Enter to exit.")
-    input()
-    sim.close()
-
 
 def main():
     """Entrypoint function of the MAL Toolbox CLI"""
@@ -124,10 +25,12 @@ def main():
     args = parser.parse_args()
 
     # Create simulator from scenario
-    simulator, sim_config = create_simulator_from_scenario(args.scenario_file)
+    cli_env = CLIEnv(args.scenario_file)
+
     if args.output_attack_graph:
-        simulator.attack_graph.save_to_file(args.output_attack_graph)
-    run_simulation(simulator, sim_config)
+        cli_env.sim.attack_graph.save_to_file(args.output_attack_graph)
+
+    cli_env.run()
 
 
 if __name__ == '__main__':
--- malsim/scenario.py
+++ malsim/scenario.py
@@ -20,27 +20,39 @@ from maltoolbox.attackgraph import AttackGraph, Attacker
 from maltoolbox.wrappers import create_attack_graph
 
 from .agents import (
+    PassiveDefender,
     PassiveAttacker,
     BreadthFirstAttacker,
     DepthFirstAttacker,
-    KeyboardAgent,
+    DepthFirstDefender,
+    BreadthFirstDefender,
+    KeyboardAttacker,
+    KeyboardDefender,
 )
 
-from .sims.mal_simulator import MalSimulator
+from .sims.mal_simulator import (
+    MalSimulator,
+    MalSimAgent,
+    MalSimAttacker,
+    MalSimDefender,
+)
 
 agent_class_name_to_class = {
     'PassiveAttacker': PassiveAttacker,
-    'BreadthFirstAttacker': BreadthFirstAttacker,
+    'PassiveDefender': PassiveDefender,
     'DepthFirstAttacker': DepthFirstAttacker,
-    'KeyboardAgent': KeyboardAgent
+    'DepthFirstDefender': DepthFirstDefender,
+    'BreadthFirstAttacker': BreadthFirstAttacker,
+    'BreadthFirstDefender': BreadthFirstDefender,
+    'KeyboardAttacker': KeyboardAttacker,
+    'KeyboardDefender': KeyboardDefender,
 }
 
 # All required fields in scenario yml file
 required_fields = [
+    'agents',
     'lang_file',
     'model_file',
-    'attacker_agent_class',
-    'defender_agent_class',
 ]
 
 # All allowed fields in scenario yml fild
@@ -192,68 +204,95 @@ def apply_scenario_node_property_rules(
                 step.extras[node_prop] = 0
 
 
-def apply_scenario_attacker_entrypoints(
-        attack_graph: AttackGraph, entry_points: dict
+def apply_attacker_entrypoints(
+        attack_graph: AttackGraph, attacker_name: str, entry_points: dict
 ) -> None:
     """Apply attacker entrypoints to attackgraph from scenario
 
-    Go through attacker entry points from scenario file and add
-    them to the referenced attacker in the attack graph
+    Creater attacker, add entrypoints to it and compromise them.
 
     Args:
     - attack_graph: the attack graph to apply entry points to
-    - entry_points: the entry points to apply
+    - attacker_name: the name to give the attacker
+    - entry_points: the entry points to apply for the attacker
     """
 
-    for attacker_name, entry_point_names in entry_points.items():
-        attacker = Attacker(
-            attacker_name, entry_points=[], reached_attack_steps=[]
-        )
-        attack_graph.add_attacker(attacker)
+    if entry_points:
+        # Override attackers in attack graph / model if
+        # entry points are defined in scenario
+        for attacker in attack_graph.attackers:
+            attack_graph.remove_attacker(attacker)
 
-        for entry_point_name in entry_point_names:
-            entry_point = attack_graph.get_node_by_full_name(entry_point_name)
-            if not entry_point:
-                raise LookupError(f"Node {entry_point_name} does not exist")
-            attacker.compromise(entry_point)
+    attacker = Attacker(
+        attacker_name, entry_points=[], reached_attack_steps=[])
+    attack_graph.add_attacker(attacker)
 
-        attacker.entry_points = list(attacker.reached_attack_steps)
+    for entry_point_name in entry_points:
+        entry_point = attack_graph.get_node_by_full_name(entry_point_name)
+        if not entry_point:
+            raise LookupError(f"Node {entry_point_name} does not exist")
+        attacker.compromise(entry_point)
+
+    attacker.entry_points = list(attacker.reached_attack_steps)
+
+    return attacker.id
 
 
-def load_scenario_simulation_config(scenario: dict) -> dict:
-    """Load configurations used in MALSimulator
-    Load parts of scenario are used for the MALSimulator
+def load_simulator_agents(
+        attack_graph: AttackGraph, scenario: dict
+    ) -> dict[str, MalSimAgent]:
+    """Load agents to be registered in MALSimulator
+
+    Create the agents from the specified classes,
+    register entrypoints for attackers.
 
     Args:
     - scenario: the scenario in question as a dict
     Return:
-    - config: a dict containing config
+    - agents: a dict containing agents and their settings
     """
 
     # Create config object which is later returned
-    config = {}
-    config['agents'] = {}
+    agents = {}
+    if agents_config := scenario.get('agents', {}):
 
-    # Currently only support one defender and attacker
-    attacker_id = "attacker"
-    defender_id = "defender"
+        for agent_id, agent_info in agents_config.items():
+            class_name = agent_info.get('agent_class')
 
-    if a_class := scenario.get('attacker_agent_class'):
-        if a_class not in agent_class_name_to_class:
-            raise LookupError(f"Agent class '{a_class}' not supported")
-        config['agents'][attacker_id] = {}
-        config['agents'][attacker_id]['type'] = 'attacker'
-        config['agents'][attacker_id]['agent_class'] = \
-            agent_class_name_to_class.get(a_class)
+            if class_name is None:
+                # Class-less agent
+                continue
 
-    if d_class := scenario.get('defender_agent_class'):
-        if d_class not in agent_class_name_to_class:
-            raise LookupError(f"Agent class '{d_class}' not supported")
-        config['agents'][defender_id] = {}
-        config['agents'][defender_id]['type'] = 'defender'
-        config['agents'][defender_id]['agent_class'] = \
-            agent_class_name_to_class.get(d_class)
-    return config
+            if class_name and class_name not in agent_class_name_to_class:
+                # Illegal class agent
+                raise LookupError(
+                    f"Agent class '{class_name}' not supported"
+                )
+
+            # Initialize the agent object
+            agent_class = agent_class_name_to_class.get(class_name)
+
+            if issubclass(agent_class, MalSimAttacker):
+                # Attacker init needs attacker entrypoints / attacker id
+                attacker_id = apply_attacker_entrypoints(
+                    attack_graph, agent_id,
+                    agents_config[agent_id].get('entry_points')
+                )
+                agent = agent_class(agent_id, attacker_id)
+
+            elif issubclass(agent_class, MalSimDefender):
+                # Defender init only needs agent id
+                agent = agent_class(agent_id)
+
+            else:
+                raise RuntimeError(
+                    f"Class {class_name} does not inherit from"
+                    "either MalSimDefender or MalSimAttacker"
+                )
+
+            agents[agent_id] = agent
+
+    return agents
 
 
 def apply_scenario_to_attack_graph(
@@ -275,17 +314,6 @@ def apply_scenario_to_attack_graph(
     rewards = scenario.get('rewards', {})
     apply_scenario_rewards(attack_graph, rewards)
 
-    # Apply attacker entrypoints to attack graph
-    entry_points = scenario.get('attacker_entry_points', {})
-    if entry_points:
-        # Override attackers in attack graph if
-        # entry points defined in scenario
-        for attacker in attack_graph.attackers:
-            attack_graph.remove_attacker(attacker)
-
-        # Apply attacker entry points from scenario
-        apply_scenario_attacker_entrypoints(attack_graph, entry_points)
-
     # Apply observability and actionability settings to attack graph
     for node_prop in ['observable', 'actionable']:
         node_prop_settings = scenario.get(node_prop + '_steps')
@@ -293,7 +321,7 @@ def apply_scenario_to_attack_graph(
             attack_graph, node_prop, node_prop_settings)
 
 
-def load_scenario(scenario_file: str) -> tuple[AttackGraph, dict]:
+def load_scenario(scenario_file: str) -> tuple[AttackGraph, dict[str, MalSimAgent]]:
     """Load a scenario from a scenario file to an AttackGraph"""
 
     with open(scenario_file, 'r', encoding='utf-8') as s_file:
@@ -307,13 +335,13 @@ def load_scenario(scenario_file: str) -> tuple[AttackGraph, dict]:
         apply_scenario_to_attack_graph(attack_graph, scenario)
 
         # Load the scenario configuration
-        scenario_config = load_scenario_simulation_config(scenario)
+        scenario_agents = load_simulator_agents(attack_graph, scenario)
 
-        return attack_graph, scenario_config
+        return attack_graph, scenario_agents
 
 
 def create_simulator_from_scenario(
-        scenario_file: str, **kwargs
+        scenario_file: str, sim_class=MalSimulator, **kwargs
     ) -> tuple[MalSimulator, dict]:
     """Creates and returns a MalSimulator created according to scenario file
 
@@ -325,22 +353,19 @@ def create_simulator_from_scenario(
     - scenario_file: the file name of the scenario
 
     Returns:
-    - MalSimulator: the resulting simulator
+    - sim: the resulting simulator
+    - agents: the agents as a dict
     """
 
-    attack_graph, conf = load_scenario(scenario_file)
+    attack_graph, scenario_agents = load_scenario(scenario_file)
 
-    sim = MalSimulator(
-        attack_graph.lang_graph,
-        attack_graph.model,
+    sim = sim_class(
         attack_graph,
         **kwargs
     )
 
-    for agent_id, agent_info in conf['agents'].items():
-        if agent_info['type'] == "attacker":
-            sim.register_attacker(agent_id, 0)
-        elif agent_info['type'] == "defender":
-            sim.register_defender(agent_id)
+    # Register agents in simulator
+    for _, agent in scenario_agents.items():
+        sim.register_agent(agent)
 
-    return sim, conf
+    return sim, scenario_agents
