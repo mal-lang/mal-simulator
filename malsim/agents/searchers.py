ff55ab5c32e3e7f5d608a7ac570a2e627fc72eb6
Nikolaos Kakouros <nkakouros@google.com>
2025-01-13 22:57:53 +0000

    Add detectors in agents

--- malsim/agents/searchers.py
+++ malsim/agents/searchers.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
+
+import json
 import logging
+import pprint
 import re
 
 from collections import deque
@@ -19,7 +22,7 @@ logger = logging.getLogger(__name__)
 class BreadthFirstAttacker(DecisionAgent):
     """A Breadth-First agent, with possible randomization at each level."""
 
-    _extend_method = "extendleft"
+    _extend_method = 'extendleft'
     # Controls where newly discovered steps will be appended to the list of
     # available actions. Currently used to differentiate between BFS and DFS
     # agents.
@@ -33,6 +36,7 @@ class BreadthFirstAttacker(DecisionAgent):
         # policy of the agent (e.g. BFS or DFS).
         'seed': None,
         # The random seed to initialize the randomness engine with.
+        'wait_factor': 0,
     }
 
     def __init__(self, agent_config: dict) -> None:
@@ -43,9 +47,9 @@ class BreadthFirstAttacker(DecisionAgent):
         """
         self.targets: deque[AttackGraphNode] = deque()
         self.current_target: Optional[AttackGraphNode] = None
-
+        self.logs: list[dict] = []
+        self.attack_graph = agent_config.pop('attack_graph')
         self.settings = self.default_settings | agent_config
-
         self.rng = np.random.default_rng(
             self.settings['seed'] or np.random.SeedSequence()
         )
@@ -56,10 +60,18 @@ class BreadthFirstAttacker(DecisionAgent):
         self._update_targets(agent.action_surface)
         self._select_next_target()
 
+        if self.current_target:
+            self._collect_logs(agent)
+
+        # TODO fix this
+        act = np.random.choice(
+            [True, False],
+            p=[1 - self.settings['wait_factor'], self.settings['wait_factor']],
+        )
+
         return self.current_target
 
     def _update_targets(self, action_surface: list[AttackGraphNode]):
-
         # action surface does not have a guaranteed order,
         # so for the agent to be deterministic we need to sort
         action_surface.sort(key=lambda n: n.id)
@@ -93,6 +105,43 @@ class BreadthFirstAttacker(DecisionAgent):
         except IndexError:
             self.current_target = None
 
+    def _collect_logs(self, observation):
+        for _, detector in self.attack_graph.nodes[
+            self.current_target
+        ].detectors.items():
+            attack_step = self.attack_graph.nodes[self.current_target]
+            log = {
+                'timestamp': observation['timestamp'],
+                '_detector': detector.name,
+                'asset': str(attack_step.asset.name),
+                'attack_step': attack_step.name,
+                'agent': self.__class__.__name__,
+                #'context': {},
+            }
+
+            for label, lgasset in detector.context.items():
+                *_, asset = (
+                    step.asset
+                    for step in self.attack_graph.attackers[0].reached_attack_steps
+                    if step.asset.type
+                    in [subasset.name for subasset in lgasset.sub_assets]
+                )
+
+                log[label] = str(asset.name)
+
+            self.logs.append(log)
+
+            logger.info('Detector triggered on %s', attack_step.full_name)
+            logger.info(pprint.pformat(log))
+
+    def terminate(self):
+        self._write_logs()
+
+    def _write_logs(self):
+        with open('logs.json', 'w') as f:
+            json.dump(self.logs, f, indent=2)
+            self.logs = []
+
 
 class DepthFirstAttacker(BreadthFirstAttacker):
-    _extend_method = "extend"
+    _extend_method = 'extend'
