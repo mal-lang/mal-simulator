cdacb5757f55bf01152fb63c6179668461767617
Joakim Loxdal <loxdal@kth.se>
2025-01-21 11:27:45 +0100

    Simplify example scenario test

--- tests/sims/test_example_scenarios.py
+++ tests/sims/test_example_scenarios.py
@@ -4,14 +4,23 @@ expected reward is given to agents
 """
 
 from malsim.scenario import create_simulator_from_scenario
-from malsim.sims import MalSimVectorizedObsEnv
 
 def test_bfs_vs_bfs_state_and_reward():
+    """
+    The point of this test is to see that a specific
+    scenario runs deterministically.
+
+    The test creates a simulator, two agents and runs them both with
+    BFS Agents against each other.
+
+    It then verifies that rewards and actions performed are what we expected.
+    """
+
     sim, agents = create_simulator_from_scenario(
         'tests/testdata/scenarios/bfs_vs_bfs_scenario.yml',
     )
-    env = MalSimVectorizedObsEnv(sim)
-    obs, infos = env.reset()
+
+    sim.reset()
 
     defender_agent_name = 'defender1'
     attacker_agent_name = 'attacker1'
@@ -31,61 +40,66 @@ def test_bfs_vs_bfs_state_and_reward():
     total_reward_defender = 0
     total_reward_attacker = 0
 
-    attacker = env.attack_graph.get_attacker_by_id(attacker_agent_info['attacker_id'])
-    attacker_actions = [env.node_to_index(n) for n in attacker.entry_points]
-    defender_actions = [env.node_to_index(n) for n in env.attack_graph.nodes
+    attacker = sim.attack_graph.get_attacker_by_id(attacker_agent_info['attacker_id'])
+    attacker_actions = [n.id for n in attacker.entry_points]
+    defender_actions = [n.id for n in sim.attack_graph.nodes
                         if n.is_enabled_defense()]
 
     while True:
-        attacker_agent_state = env.get_agent(attacker_agent_info['name'])
+        # Run the simulation until agents are terminated/truncated
+
+        # Select attacker node
+        attacker_agent_state = sim.get_agent(attacker_agent_info['name'])
         attacker_node = attacker_agent.get_next_action(
             attacker_agent_state
         )
-        defender_agent_state = env.get_agent(defender_agent_info['name'])
+
+        # Select defender node
+        defender_agent_state = sim.get_agent(defender_agent_info['name'])
         defender_node = defender_agent.get_next_action(
             defender_agent_state
         )
 
-        attacker_action = (0, None)
-        defender_action = (0, None)
-
-        if attacker_node:
-            node_index = env.node_to_index(attacker_node)
-            attacker_action = (1, node_index)
-            attacker_actions.append(node_index)
-
-        if defender_node:
-            node_index = env.node_to_index(defender_node)
-            defender_action = (1, node_index)
-            defender_actions.append(node_index)
-
+        # Step
         actions = {
-            defender_agent_name: defender_action,
-            attacker_agent_name: attacker_action
+            defender_agent_name: [defender_node] or [],
+            attacker_agent_name: [attacker_node] or []
         }
-        obs, rew, trunc, term, infos = env.step(actions)
+        performed_actions, _ = sim.step(actions)
 
-        total_reward_defender += rew[defender_agent_name]
-        total_reward_attacker += rew[attacker_agent_name]
-        if term[defender_agent_name] or term[attacker_agent_name]:
+        # If actions were performed, add them to respective list
+        if attacker_node and attacker_node in performed_actions:
+            attacker_actions.append(attacker_node.id)
+
+        if defender_node and defender_node in performed_actions:
+            defender_actions.append(defender_node.id)
+
+        total_reward_defender += defender_agent_state.reward
+        total_reward_attacker += attacker_agent_state.reward
+
+        # Break simulation if trunc or term
+        if defender_agent_state.terminated or attacker_agent_state.terminated:
             break
-
-        if trunc[defender_agent_name] or trunc[attacker_agent_name]:
+        if defender_agent_state.truncated or attacker_agent_state.truncated:
             break
+    
+    # Make sure the actions performed were as expected
+    assert attacker_actions == [370, 371, 398, 372, 399, 400, 401, 402, 373, 403, 422, 324, 374, 423]
+    assert defender_actions == [71, 284, 365, 366, 393, 394, 443, 444, 471, 472, 473, 512, 0, 31, 102, 142, 173, 213, 244, 287, 315, 367, 368, 395, 396, 421]
 
-    assert attacker_actions == [328, 329, 353, 330, 354, 355, 356, 357, 331, 358, 375, 283, 332, 376, 377]
-    assert defender_actions == [68, 249, 324, 325, 349, 350, 396, 397, 421, 422, 423, 457, 0, 31, 88, 113, 144, 181, 212, 252, 276, 326, 327, 351, 352, 374]
+    for step_id in attacker_actions:
+        # Make sure that all attacker actions led to compromise
+        node = sim.attack_graph.get_node_by_id(step_id)
+        assert node.is_compromised()
 
-    for step_index in attacker_actions:
-        node = env.index_to_node(step_index)
-        if node.is_compromised():
-            assert obs[defender_agent_name]['observed_state'][step_index]
+    for step_id in defender_actions:
+        # Make sure that all defender actions let to defense enabled
+        node = sim.attack_graph.get_node_by_id(step_id)
+        assert node.is_enabled_defense()
 
-    for step_index in defender_actions:
-        assert obs[defender_agent_name]['observed_state'][step_index]
-
-    assert rew[attacker_agent_name] == 0
-    assert rew[defender_agent_name] == -31
+    # Verify rewards in latest run and total rewards
+    assert attacker_agent_state.reward == 0
+    assert defender_agent_state.reward == -31
 
     assert total_reward_attacker == 0
     assert total_reward_defender == -307
