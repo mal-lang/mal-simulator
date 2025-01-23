73a174606da96eea386dad7382fca4bdb493ffea
Joakim Loxdal <loxdal@kth.se>
2025-01-23 11:52:23 +0100

    Add heuristic agents that tries to predict the future by looking at parents of its children

--- malsim/agents/__init__.py
+++ malsim/agents/__init__.py
@@ -2,7 +2,9 @@ from .decision_agent import DecisionAgent
 from .passive_agent import PassiveAgent
 from .keyboard_input import KeyboardAgent
 from .searchers import BreadthFirstAttacker, DepthFirstAttacker
-from .heuristic_agent import ShutdownCompromisedMachinesDefender
+from .heuristic_agent import (
+    DefendCompromisedDefender, DefendFutureCompromisedDefender
+)
 
 __all__ = [
     'PassiveAgent',
@@ -10,5 +12,6 @@ __all__ = [
     'KeyboardAgent',
     'BreadthFirstAttacker',
     'DepthFirstAttacker',
-    'ShutdownCompromisedMachinesDefender'
+    'DefendCompromisedDefender',
+    'DefendFutureCompromisedDefender'
 ]
