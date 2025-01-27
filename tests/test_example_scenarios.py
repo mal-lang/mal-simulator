9942b36fafa6c24187434ca5c46c9df785388b9a
Joakim Loxdal <loxdal@kth.se>
2025-01-27 10:51:48 +0100

    Use names instead of ids in scenario example test and update to new action order

--- tests/test_example_scenarios.py
+++ tests/test_example_scenarios.py
@@ -1,47 +1,48 @@
 from malsim.scenario import create_simulator_from_scenario
 
+
 def test_bfs_vs_bfs_state_and_reward():
     sim, sim_config = create_simulator_from_scenario(
-        'tests/testdata/scenarios/bfs_vs_bfs_scenario.yml')
+        "tests/testdata/scenarios/bfs_vs_bfs_scenario.yml"
+    )
     obs, infos = sim.reset()
 
     attacker_agent_id = next(iter(sim.get_attacker_agents()))
     defender_agent_id = next(iter(sim.get_defender_agents()), None)
 
     # Initialize defender and attacker according to classes
-    defender_class = sim_config['agents'][defender_agent_id]['agent_class']
+    defender_class = sim_config["agents"][defender_agent_id]["agent_class"]
     defender_agent = defender_class({})
 
-    attacker_class = sim_config['agents'][attacker_agent_id]['agent_class']
+    attacker_class = sim_config["agents"][attacker_agent_id]["agent_class"]
     attacker_agent = attacker_class({})
 
     total_reward_defender = 0
     total_reward_attacker = 0
 
     attacker = sim.attack_graph.attackers[0]
-    attacker_actions = [sim._id_to_index[n.id] for n in attacker.entry_points]
-    defender_actions = [sim._id_to_index[n.id]
-                        for n in sim.attack_graph.nodes
-                        if n.is_enabled_defense()]
+    attacker_actions = [n.full_name for n in attacker.entry_points]
+    defender_actions = [
+        n.full_name for n in sim.attack_graph.nodes if n.is_enabled_defense()
+    ]
 
     while True:
         defender_action = defender_agent.compute_action_from_dict(
-                          obs[defender_agent_id],
-                          infos[defender_agent_id]["action_mask"])
+            obs[defender_agent_id], infos[defender_agent_id]["action_mask"]
+        )
 
         attacker_action = attacker_agent.compute_action_from_dict(
-                          obs[attacker_agent_id],
-                          infos[attacker_agent_id]["action_mask"])
+            obs[attacker_agent_id], infos[attacker_agent_id]["action_mask"]
+        )
 
         if attacker_action[0]:
-            attacker_actions.append(int(attacker_action[1]))
+            attacker_node = sim.action_to_node(attacker_action)
+            attacker_actions.append(attacker_node.full_name)
         if defender_action[0]:
-            defender_actions.append(int(defender_action[1]))
+            defender_node = sim.action_to_node(defender_action)
+            defender_actions.append(defender_node.full_name)
 
-        actions = {
-            'defender': defender_action,
-            'attacker': attacker_action
-        }
+        actions = {"defender": defender_action, "attacker": attacker_action}
         obs, rewards, terminated, truncated, infos = sim.step(actions)
 
         total_reward_defender += rewards.get(defender_agent_id, 0)
@@ -50,16 +51,62 @@ def test_bfs_vs_bfs_state_and_reward():
         if terminated[defender_agent_id] or terminated[attacker_agent_id]:
             break
 
-    assert attacker_actions == [328, 329, 353, 330, 354, 355, 356, 331, 357, 283, 332, 375, 358, 376, 377]
-    assert defender_actions == [68, 249, 324, 325, 349, 350, 396, 397, 421, 422, 423, 457, 0, 31, 88, 113, 144, 181, 212, 252, 276, 326, 327, 351, 352, 374]
+    assert attacker_actions == [
+        "Credentials:6:attemptCredentialsReuse",
+        "Credentials:6:credentialsReuse",
+        "Credentials:7:attemptCredentialsReuse",
+        "Credentials:6:attemptUse",
+        "Credentials:7:credentialsReuse",
+        "Credentials:7:attemptUse",
+        "Credentials:7:use",
+        "Credentials:7:attemptPropagateOneCredentialCompromised",
+        "Credentials:7:propagateOneCredentialCompromised",
+        "User:12:oneCredentialCompromised",
+        "User:12:passwordReuseCompromise",
+        "Credentials:9:attemptCredentialsReuse",
+        "Credentials:10:attemptCredentialsReuse",
+        "Credentials:9:credentialsReuse",
+        "Credentials:9:attemptUse",
+    ]
+    assert defender_actions == [
+        "Program 1:notPresent",
+        "IDPS 1:effectiveness",
+        "Credentials:6:notDisclosed",
+        "Credentials:6:notGuessable",
+        "Credentials:7:notDisclosed",
+        "Credentials:7:notGuessable",
+        "Credentials:9:notDisclosed",
+        "Credentials:9:notGuessable",
+        "Credentials:10:notDisclosed",
+        "Credentials:10:notGuessable",
+        "Credentials:10:unique",
+        "User:12:noRemovableMediaUsage",
+        "OS App:notPresent",
+        "OS App:supplyChainAuditing",
+        "Program 1:supplyChainAuditing",
+        "Program 2:notPresent",
+        "Program 2:supplyChainAuditing",
+        "IDPS 1:notPresent",
+        "IDPS 1:supplyChainAuditing",
+        "SoftwareVulnerability:4:notPresent",
+        "Data:5:notPresent",
+        "Credentials:6:unique",
+        "Credentials:6:notPhishable",
+        "Credentials:7:unique",
+        "Credentials:7:notPhishable",
+        "Identity:8:notPresent",
+    ]
 
-    for step_index in attacker_actions:
-        node = sim.attack_graph.get_node_by_id(sim._index_to_id[step_index])
+    for step_fullname in attacker_actions:
+        node = sim.attack_graph.get_node_by_full_name(step_fullname)
         if node.is_compromised():
-            assert obs[defender_agent_id]['observed_state'][step_index]
+            node_index = sim.node_to_index(node)
+            assert obs[defender_agent_id]["observed_state"][node_index]
 
-    for step_index in defender_actions:
-        assert obs[defender_agent_id]['observed_state'][step_index]
+    for step_fullname in defender_actions:
+        node = sim.attack_graph.get_node_by_full_name(step_fullname)
+        node_index = sim.node_to_index(node)
+        assert obs[defender_agent_id]["observed_state"][node_index]
 
     assert rewards[attacker_agent_id] == 0
     assert rewards[defender_agent_id] == -31
