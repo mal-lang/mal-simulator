1d3853173ca0ce1473a72738a4af40a0cf87590e
Nikolaos Kakouros <nkak@kth.se>
2025-02-25 20:58:44 +0200

    Remove unused malsim setting

--- malsim/mal_simulator.py
+++ malsim/mal_simulator.py
@@ -124,12 +124,6 @@ class MalSimulatorSettings():
     # - Leave the node/step compromised even after it becomes untraversable
     uncompromise_untraversable_steps: bool = False
 
-    # cumulative_defender_obs
-    # - Defender sees the status of the whole attack graph if set to True
-    # otherwise:
-    # - Defender only sees the status of nodes changed in the current step
-    cumulative_defender_obs: bool = True
-
 
 class MalSimulator():
     """A MAL Simulator that works on the AttackGraph
