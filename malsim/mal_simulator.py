65db799e5aacb531db9fb6b11365f5d38e197e77
Andrei Buhaiu <buhaiu@kth.se>
2025-02-26 11:38:12 +0100

    Re-enable cummulative step action surface removals

--- malsim/mal_simulator.py
+++ malsim/mal_simulator.py
@@ -427,13 +427,13 @@ class MalSimulator():
 
         for defender_agent in self._get_defender_agents():
             # Remove enabled defenses from all defenders action surface
-            defender_agent.step_action_surface_removals = enabled_defenses
+            defender_agent.step_action_surface_removals |= enabled_defenses
             defender_agent.action_surface -= enabled_defenses
 
         for attacker_agent in self._get_attacker_agents():
             # Remove attack steps made unviable from all attackers
             # action surfaces if they were part of it
-            attacker_agent.step_action_surface_removals = (
+            attacker_agent.step_action_surface_removals |= (
                 attacker_agent.action_surface & attack_steps_made_unviable
             )
             attacker_agent.action_surface -= attack_steps_made_unviable
