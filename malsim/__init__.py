37b5aac928ca3376206597c2414eb61c73e5bad0
Joakim Loxdal <loxdal@kth.se>
2025-03-11 14:55:39 +0100

    Bump version to 0.2.4

--- malsim/__init__.py
+++ malsim/__init__.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-# MAL Simulator v0.2.3
+# MAL Simulator v0.2.4
 # Copyright 2025, Andrei Buhaiu.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,7 +24,7 @@ MAL Simulator
 """
 
 __title__ = "malsim"
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __authors__ = ["Andrei Buhaiu", "Joakim Loxdal", "Jakob Nyberg", "Nikolaos Kakouros"]
 __license__ = "Apache 2.0"
 __docformat__ = "restructuredtext en"
