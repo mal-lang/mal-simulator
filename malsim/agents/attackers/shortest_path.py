fdca5c847a424f5629ef1e2dd8772851049c01ad
Joakim Loxdal <loxdal@kth.se>
2025-09-25 13:37:39 +0200

    Add experimental warning

--- malsim/agents/attackers/shortest_path.py
+++ malsim/agents/attackers/shortest_path.py
@@ -2,12 +2,15 @@
 from __future__ import annotations
 from typing import Any, TYPE_CHECKING, Optional
 import random
+import logging
 
 from ..utils.path_finding import get_shortest_paths_for_attacker
 
 if TYPE_CHECKING:
     from malsim.mal_simulator import MalSimAttackerState
     from maltoolbox.attackgraph import AttackGraphNode
+
+logger = logging.getLogger(__name__)
 
 class ShortestPathAttacker:
     """
@@ -19,6 +22,8 @@ class ShortestPathAttacker:
     """
 
     def __init__(self, agent_config: dict[str, Any]):
+        print("Note: ShortestPathAttacker is in experimental mode")
+        logger.warning("This agent is in experimental mode")
         seed = agent_config.get("seed")
         self.rng = random.Random(seed)
 
