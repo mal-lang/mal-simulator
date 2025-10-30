4af70e95fe45be0e05fab279d518a6939108fe79
sandorstormen <sandor.berglund@gmail.com>
2025-10-30 15:36:44 +0100

    Update environment initialization to include GraphDefenderEnv
    - Added GraphDefenderEnv to the environment registration in `__init__.py`.
    - Ensured compatibility with existing environment structure and registration methods.

--- malsim/envs/__init__.py
+++ malsim/envs/__init__.py
@@ -1,6 +1,6 @@
 from .malsim_vectorized_obs_env import MalSimVectorizedObsEnv
 from .gym_envs import AttackerEnv, DefenderEnv, register_envs # type: ignore
-from .graph.graph_env import GraphAttackerEnv, register_graph_envs
+from .graph.graph_env import GraphAttackerEnv, GraphDefenderEnv, register_graph_envs
 
 # not needed, used to silence ruff F401
 __all__ = [
@@ -9,5 +9,6 @@ __all__ = [
     "DefenderEnv",
     "register_envs",
     "GraphAttackerEnv",
+    "GraphDefenderEnv",
     "register_graph_envs",
 ]
