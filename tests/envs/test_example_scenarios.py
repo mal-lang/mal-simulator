e33d9c523107b8e2c5949777b49fdfc19b86eba2
sandorstormen <sandor.berglund@gmail.com>
2025-12-02 14:51:18 +0100

    test: update example scenarios with new actions and reward assertions

--- tests/envs/test_example_scenarios.py
+++ tests/envs/test_example_scenarios.py
@@ -239,34 +239,43 @@ def test_bfs_vs_bfs_state_and_reward_per_step_ttc() -> None:
         'Program 1:accessNetworkAndConnections',
         'Program 1:attemptModify',
         'Program 1:specificAccess',
+        'ConnectionRule:1:attemptAccessNetworksUninspected',
+        'ConnectionRule:1:attemptConnectToApplicationsUninspected',
         'ConnectionRule:1:attemptAccessNetworksInspected',
         'ConnectionRule:1:attemptConnectToApplicationsInspected',
-        'ConnectionRule:1:successfulAccessNetworksInspected',
         'ConnectionRule:1:bypassRestricted',
+        'ConnectionRule:1:successfulAccessNetworksInspected',
         'ConnectionRule:1:connectToApplicationsInspected',
         'ConnectionRule:1:accessNetworksInspected',
         'Program 1:networkConnectInspected',
         'Network:2:accessInspected',
-        'Program 1:networkConnect',
         'Program 1:specificAccessNetworkConnect',
-        'Network:2:deny',
-        'Network:2:networkForwardingInspected',
+        'Program 1:networkConnect',
         'Network:2:accessNetworkData',
+        'Network:2:networkForwardingInspected',
+        'Network:2:deny',
         'ConnectionRule:3:attemptConnectToApplicationsInspected',
-        'ConnectionRule:1:attemptDeny',
-        'ConnectionRule:3:attemptDeny',
-        'ConnectionRule:3:attemptAccessNetworksInspected',
         'Network:2:attemptEavesdrop',
         'Network:2:attemptAdversaryInTheMiddle',
-        'ConnectionRule:3:connectToApplicationsInspected',
+        'ConnectionRule:3:attemptAccessNetworksInspected',
+        'ConnectionRule:1:attemptDeny',
+        'ConnectionRule:3:attemptDeny',
+        'ConnectionRule:3:bypassPayloadInspection',
         'ConnectionRule:3:bypassRestricted',
+        'ConnectionRule:3:connectToApplicationsInspected',
+        'Network:2:successfulEavesdrop',
+        'Network:2:bypassEavesdropDefense',
+        'Network:2:bypassAdversaryInTheMiddleDefense',
+        'Network:2:successfulAdversaryInTheMiddle',
+        'ConnectionRule:3:successfulAccessNetworksInspected',
         'ConnectionRule:1:deny',
         'ConnectionRule:3:deny',
-        'ConnectionRule:3:successfulAccessNetworksInspected',
         'Program 2:networkConnectInspected',
+        'Network:2:eavesdrop',
+        'Network:2:adversaryInTheMiddle',
+        'ConnectionRule:3:accessNetworksInspected',
         'Program 1:denyFromNetworkingAsset',
         'Program 2:denyFromNetworkingAsset',
-        'ConnectionRule:3:accessNetworksInspected',
         'Program 2:specificAccessNetworkConnect',
         'Program 2:networkConnect',
         'Program 2:attemptDeny',
@@ -303,7 +312,7 @@ def test_bfs_vs_bfs_state_and_reward_per_step_ttc() -> None:
     assert sim.agent_reward(defender_state.name) == -19
 
     assert total_reward_attacker == -attacker_failed_steps
-    assert total_reward_defender == -3710
+    assert total_reward_defender == -15927.0
 
 
 def test_bfs_vs_bfs_state_and_reward_per_step_effort_based() -> None:
@@ -365,36 +374,12 @@ def test_bfs_vs_bfs_state_and_reward_per_step_effort_based() -> None:
         total_reward_defender += sim.agent_reward(defender_state.name)
         total_reward_attacker += sim.agent_reward(attacker_state.name)
 
-    assert sim.cur_iter == 25
+    assert sim.cur_iter == 11
 
     # Make sure the actions performed were as expected
-    assert attacker_actions == [
-        'Program 1:attemptApplicationRespondConnectThroughData',
-        'Program 1:attemptRead',
-        'Program 1:attemptDeny',
-        'Program 1:accessNetworkAndConnections',
-        'Program 1:attemptModify',
-        'Program 1:specificAccess',
-        'ConnectionRule:1:attemptAccessNetworksUninspected',
-        'ConnectionRule:1:attemptConnectToApplicationsUninspected',
-        'ConnectionRule:1:attemptAccessNetworksInspected',
-        'ConnectionRule:1:attemptConnectToApplicationsInspected',
-        'ConnectionRule:1:bypassPayloadInspection',
-    ]
+    assert attacker_actions == ['Program 1:attemptApplicationRespondConnectThroughData', 'Program 1:attemptRead', 'Program 1:attemptDeny', 'Program 1:accessNetworkAndConnections', 'Program 1:attemptModify', 'Program 1:specificAccess', 'ConnectionRule:1:attemptAccessNetworksUninspected', 'ConnectionRule:1:attemptConnectToApplicationsUninspected', 'ConnectionRule:1:attemptAccessNetworksInspected']
 
-    assert defender_actions == [
-        'Network:2:adversaryInTheMiddleDefense',
-        'ConnectionRule:3:payloadInspection',
-        'Program 2:supplyChainAuditing',
-        'ConnectionRule:3:restricted',
-        'Network:2:eavesdropDefense',
-        'ConnectionRule:1:payloadInspection',
-        'Program 1:notPresent',
-        'Network:2:networkAccessControl',
-        'Program 1:supplyChainAuditing',
-        'Program 2:notPresent',
-        'ConnectionRule:1:restricted',
-    ]
+    assert defender_actions == ['Network:2:adversaryInTheMiddleDefense', 'ConnectionRule:3:payloadInspection', 'Program 2:supplyChainAuditing', 'ConnectionRule:3:restricted', 'Network:2:eavesdropDefense', 'ConnectionRule:1:payloadInspection', 'Program 1:notPresent', 'Network:2:networkAccessControl', 'Program 1:supplyChainAuditing', 'Program 2:notPresent', 'ConnectionRule:1:restricted']
     for step_id in attacker_actions:
         # Make sure that all attacker actions led to compromise
         node = sim.attack_graph.get_node_by_full_name(step_id)
@@ -413,7 +398,7 @@ def test_bfs_vs_bfs_state_and_reward_per_step_effort_based() -> None:
     assert sim.agent_reward(defender_state.name) == -19
 
     assert total_reward_attacker == -attacker_failed_steps
-    assert total_reward_defender == -366.0
+    assert total_reward_defender == -100.0
 
 
 def test_bfs_vs_bfs_state_and_reward_expected_value_ttc() -> None:
@@ -477,36 +462,12 @@ def test_bfs_vs_bfs_state_and_reward_expected_value_ttc() -> None:
         total_reward_defender += sim.agent_reward(defender_state.name)
         total_reward_attacker += sim.agent_reward(attacker_state.name)
 
-    assert sim.cur_iter == 111
+    assert sim.cur_iter == 11
 
     # Make sure the actions performed were as expected
-    assert attacker_actions == [
-        'Program 1:attemptApplicationRespondConnectThroughData',
-        'Program 1:attemptRead',
-        'Program 1:attemptDeny',
-        'Program 1:accessNetworkAndConnections',
-        'Program 1:attemptModify',
-        'Program 1:specificAccess',
-        'ConnectionRule:1:attemptAccessNetworksUninspected',
-        'ConnectionRule:1:attemptConnectToApplicationsUninspected',
-        'ConnectionRule:1:attemptAccessNetworksInspected',
-        'ConnectionRule:1:attemptConnectToApplicationsInspected',
-        'ConnectionRule:1:bypassPayloadInspection',
-    ]
+    assert attacker_actions == ['Program 1:attemptApplicationRespondConnectThroughData', 'Program 1:attemptRead', 'Program 1:attemptDeny', 'Program 1:accessNetworkAndConnections', 'Program 1:attemptModify', 'Program 1:specificAccess', 'ConnectionRule:1:attemptAccessNetworksUninspected', 'ConnectionRule:1:attemptConnectToApplicationsUninspected', 'ConnectionRule:1:attemptAccessNetworksInspected']
 
-    assert defender_actions == [
-        'Network:2:adversaryInTheMiddleDefense',
-        'ConnectionRule:3:payloadInspection',
-        'Program 2:supplyChainAuditing',
-        'ConnectionRule:3:restricted',
-        'Network:2:eavesdropDefense',
-        'ConnectionRule:1:payloadInspection',
-        'Program 1:notPresent',
-        'Network:2:networkAccessControl',
-        'Program 1:supplyChainAuditing',
-        'Program 2:notPresent',
-        'ConnectionRule:1:restricted',
-    ]
+    assert defender_actions == ['Network:2:adversaryInTheMiddleDefense', 'ConnectionRule:3:payloadInspection', 'Program 2:supplyChainAuditing', 'ConnectionRule:3:restricted', 'Network:2:eavesdropDefense', 'ConnectionRule:1:payloadInspection', 'Program 1:notPresent', 'Network:2:networkAccessControl', 'Program 1:supplyChainAuditing', 'Program 2:notPresent', 'ConnectionRule:1:restricted']
     for step_id in attacker_actions:
         # Make sure that all attacker actions led to compromise
         node = sim.attack_graph.get_node_by_full_name(step_id)
@@ -525,7 +486,7 @@ def test_bfs_vs_bfs_state_and_reward_expected_value_ttc() -> None:
     assert sim.agent_reward(defender_state.name) == -19
 
     assert total_reward_attacker == -attacker_failed_steps
-    assert total_reward_defender == -2000.0
+    assert total_reward_defender == -100.0
 
 
 def test_traininglang_advanced_agents() -> None:
