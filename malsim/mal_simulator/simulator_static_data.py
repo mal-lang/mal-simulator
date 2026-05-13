f52955385cdeb497c54e4e1f5e8827a4b6207cb0
mrkickling <loxdalen@gmail.com>
2026-05-13 15:52:43 +0200

    ruff

--- malsim/mal_simulator/simulator_static_data.py
+++ malsim/mal_simulator/simulator_static_data.py
@@ -8,11 +8,7 @@ class MALSimulatorStaticData(NamedTuple):
     attack_graph: AttackGraph
     sim_settings: MalSimulatorSettings
     rewards: dict[str, float] | dict[AttackGraphNode, float] | None = None
-    false_positive_rates: dict[str, float] | dict[AttackGraphNode, float] | None = (
-        None
-    )
-    false_negative_rates: dict[str, float] | dict[AttackGraphNode, float] | None = (
-        None
-    )
+    false_positive_rates: dict[str, float] | dict[AttackGraphNode, float] | None = None
+    false_negative_rates: dict[str, float] | dict[AttackGraphNode, float] | None = None
     node_actionabilities: dict[str, bool] | dict[AttackGraphNode, bool] | None = None
     node_observabilities: dict[str, bool] | dict[AttackGraphNode, bool] | None = None
