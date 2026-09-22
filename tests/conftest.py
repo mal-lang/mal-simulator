8cec6125b667114e94eb4bb71e84848a56cc6227
sandorstormen <sandor.berglund@gmail.com>
2026-09-22 05:20:39 +0200

    tests: Add tests for DynaMAL-like features

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,8 @@ from maltoolbox.language import (
     LanguageGraphAsset,
 )
 
+from malsim.scenario.scenario import Scenario
+
 ## Helpers
 
 
@@ -113,3 +115,96 @@ def dummy_lang_graph(corelang_lang_graph: LanguageGraph) -> LanguageGraph:
     dummy_asset.attack_steps['DummyNotExistAttackStep'] = dummy_exist_attack_step_node
 
     return lang_graph
+
+
+@pytest.fixture
+def wiperLang_lang_graph() -> LanguageGraph:
+    """Fixture that returns the wiperLang language specification as dict"""
+    mal_spec_file_path = path_testdata('langs/wiperLang.mal')
+    return LanguageGraph.from_mal_spec(mal_spec_file_path)
+
+
+@pytest.fixture
+def wiperLang_model(wiperLang_lang_graph: LanguageGraph) -> Model:
+    """Fixture that generates an example model for wiperLang"""
+
+    model_file_path = path_testdata('models/wiper_model.yml')
+    return Model.load_from_file(model_file_path, wiperLang_lang_graph)
+
+
+@pytest.fixture
+def wiperLang_attack_graph(
+    wiperLang_model: Model, wiperLang_lang_graph: LanguageGraph
+) -> AttackGraph:
+    """Fixture that generates an attack graph for wiperLang"""
+
+    return AttackGraph(wiperLang_lang_graph, wiperLang_model)
+
+
+@pytest.fixture
+def wiperLang_scenario() -> Scenario:
+    """Fixture that generates a scenario for wiperLang"""
+    scenario_file_path = path_testdata('scenarios/wiper_scenario.yml')
+    return Scenario.load_from_file(scenario_file_path)
+
+
+@pytest.fixture
+def dynamic_add_remove_scenario() -> Scenario:
+    """Fixture that generates a scenario for dynamic_add_remove"""
+    scenario_file_path = path_testdata('scenarios/dynamic_add_remove.yml')
+    return Scenario.load_from_file(scenario_file_path)
+
+
+@pytest.fixture
+def int_dynamic_test_lang6_scenario() -> Scenario:
+    """Fixture for the intDynamicTestLang6 example (intermediate)"""
+    scenario_file_path = path_testdata(
+        'scenarios/dynamal_example_scenarios/intermediate/'
+        'intDynamicTestLang6_scenario.yml'
+    )
+    return Scenario.load_from_file(scenario_file_path)
+
+
+@pytest.fixture
+def int_dynamic_test_lang3_scenario() -> Scenario:
+    """Fixture for the intDynamicTestLang3 example (intermediate)"""
+    scenario_file_path = path_testdata(
+        'scenarios/dynamal_example_scenarios/intermediate/'
+        'intDynamicTestLang3_scenario.yml'
+    )
+    return Scenario.load_from_file(scenario_file_path)
+
+
+@pytest.fixture
+def int_dynamic_test_lang12_scenario() -> Scenario:
+    """Fixture for the intDynamicTestLang12 example (intermediate)"""
+    scenario_file_path = path_testdata(
+        'scenarios/dynamal_example_scenarios/intermediate/'
+        'intDynamicTestLang12_scenario.yml'
+    )
+    return Scenario.load_from_file(scenario_file_path)
+
+
+@pytest.fixture
+def easy_ransomware_lang_scenario() -> Scenario:
+    """Fixture for the easyRansomwareLang example (basic)"""
+    scenario_file_path = path_testdata(
+        'scenarios/dynamal_example_scenarios/basic/easyRansomwareLang_scenario.yml'
+    )
+    return Scenario.load_from_file(scenario_file_path)
+
+
+@pytest.fixture
+def dynamic_remove_many_assoc_scenario() -> Scenario:
+    """Fixture for the dynamic_remove_many_assoc example"""
+    scenario_file_path = path_testdata(
+        'scenarios/dynamic_remove_many_assoc_scenario.yml'
+    )
+    return Scenario.load_from_file(scenario_file_path)
+
+
+@pytest.fixture
+def rand_multiplicity_scenario() -> Scenario:
+    """Fixture for the multiplicity example scenario"""
+    scenario_file_path = path_testdata('scenarios/rand_multiplicity_scenario.yml')
+    return Scenario.load_from_file(scenario_file_path)
